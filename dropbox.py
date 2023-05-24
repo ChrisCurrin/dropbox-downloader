8b73be4afe639d667fdf38ca7165f7ac71750dd2
Christopher Currin <chris.crisur@gmail.com>
2023-05-24 13:18:17 +0200

    log: increase logs

--- dropbox.py
+++ dropbox.py
@@ -130,10 +130,13 @@ def downloadFile(suppliedLink: str, destination: str, unzip=False, retain_zip=Fa
             directoryPath = os.path.join(destination, directoryName)
             os.makedirs(directoryPath, exist_ok=True)
             zipFile.extractall(directoryPath)
-
+        
+        logger.info(f"Extracted {filePath} to {directoryPath}")
+        
         # check if zip files should be deleted
         if not retain_zip:
             os.remove(filePath)
+            logger.info(f"Removed {filePath}")
 
 
 def downloadFiles(suppliedLinks: list[str], destination, unzip=False, retain_zip=False):
